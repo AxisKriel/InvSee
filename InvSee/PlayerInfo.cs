bd826db870569b8bbee567ce20e8f35e193188c0
Marcus Koller <marcus101rr@gmail.com>
2016-08-18 14:48:45 -0400

    Indentation fix & added userid

--- InvSee/PlayerInfo.cs
+++ InvSee/PlayerInfo.cs
@@ -2,28 +2,29 @@
 
 namespace InvSee
 {
-	public class PlayerInfo
-	{
-		public const string KEY = "InvSee_Data";
+    public class PlayerInfo
+    {
+        public const string KEY = "InvSee_Data";
 
-		public PlayerData Backup { get; set; }
+        public PlayerData Backup { get; set; }
 
-		public string CopyingUserName { get; set; }
+        public string CopyingUserName { get; set; }
+        public int UserID { get; set; }
 
-		public PlayerInfo()
-		{
-			Backup = null;
-		}
+        public PlayerInfo()
+        {
+            Backup = null;
+        }
 
-		public bool Restore(TSPlayer player)
-		{
-			if (Backup == null)
-				return false;
+        public bool Restore(TSPlayer player)
+        {
+            if (Backup == null)
+                return false;
 
-			Backup.RestoreCharacter(player);
-			Backup = null;
-			CopyingUserName = "";
-			return true;
-		}
-	}
+            Backup.RestoreCharacter(player);
+            Backup = null;
+            CopyingUserName = "";
+            return true;
+        }
+    }
 }
